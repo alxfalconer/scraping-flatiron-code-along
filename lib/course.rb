0b43e7bb0624072c089d9739cf6dd61dbafc4c9c
Alexander <al.x.falconer@gmail.com>
2020-12-29 18:41:32 -0500

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,18 @@
-
+class Course
+    attr_accessor :title, :schedule, :description
+  
+    @all = []
+  
+    class << self
+      attr_accessor :all
+    end
+  
+    def initialize
+      self.class.all << self
+    end
+  
+    def self.reset_all
+      self.all = []
+    end
+end
 
